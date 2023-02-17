3c09ad21bcedc2eabb338eba668ee10c12e9413f
roberto <roberto.wbb@hotmail.com>
2023-02-16 22:42:01 -0600

    I did the random madlibs

---
+++ Roberto/adv_madlibs.py/sample_madlibs/code.py
@@ -0,0 +1,19 @@
+def madlib():
+    body_part = input("Body Part: ")
+    verb = input("Verb: ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    adj5 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun_plural_1 = input("Noun (Plural): ")
+    noun_plural_2 = input("Noun (Plural): ")
+
+    madlib = f"I love computer programing because it's {adj1}! The journey to becoming a /n/ good programmer starts with hope in your mind and a dream in your {body_part}. Through blood, /n/ sweat, and {noun_plural_1}, hopefully it never ends. Yes, once you learn to {verb}, it becomes /n/ a part of your life identity and you can become a super {adj2} hacker. Knowledge of programming /n/ lets you take control of your {noun1}. You can create your own personal {noun_plural_2}, anything /n/ from developing {adj3} software to analyzing data and making predictions about the {noun2}. You can /n/ maybe even recreate Jarvis and make him extra {adj4}. I hope you'll start your {adj5} journey by / coding with kylie :)"
+
+    print(madlib)
+
+
+
