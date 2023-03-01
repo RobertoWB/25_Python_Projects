ca2b942d63c7566b061b0bbb893800fca4686923
roberto <roberto.wbb@hotmail.com>
2023-02-28 22:07:50 -0600

    I added my own version of rock-paper-scissors

--- Roberto/rock_paper_scissors.py
+++ Roberto/rock_paper_scissors.py
@@ -20,6 +20,39 @@ def is_win(player, opponent):
     print(f'{player} vs {opponent}')
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+ 
+
+def play_until_win():
+    lost = True
+    cont = 1
+    while lost == True:
+        user = input("What's your choise? 'r' for rock, 'p' for paper, 's' for scissors: ")
+        computer = random.choice(['r','p','s'])
+        print(f'{user} vs {computer}')
+        if(user == 's' and computer == 'r') or (user == 'p' and computer == 's') or (user == 'r' and computer == 'p'):
+            print('You lost!!, try again')
+            lost = True
+            cont += 1
+        elif user == computer:
+            print('This was a Tie, try again')
+            lost = True
+            cont += 1
+        else:
+            if cont == 1:
+                print ('One match one win, what a lucky guy')
+                lost = False 
+            else:
+                print(f'You finally won, it took you {cont} matches to win')
+                lost = False
+
+play_until_win()
+
+        
 
 
-print(play())
+
+
+
+
+
+
